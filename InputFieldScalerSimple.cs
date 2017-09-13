961465055644174c63fd94f0ed2682f9eb9b4862
sakuraiike <sakurai1003@live.cn>
2017-09-13 10:31:17 +0800

    简化版本
    InputField本身已经计算了合适的大小 无需在当前脚本再次计算

---
+++ InputFieldScalerSimple.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[RequireComponent(typeof(InputField))]
+public class InputFieldScalerSimple : MonoBehaviour {
+    public bool keepOriginalMinSize=true;
+    private Vector2 originalSize;
+    public Vector2 currentSize;
+    private InputField _inputField;
+    private InputField inputField{
+        get{
+            return _inputField??(_inputField=this.GetComponent<InputField>());
+        }
+    }
+    public void OnValueChanged(string value){
+
+        if(this.keepOriginalMinSize)
+            this.rectTransform.SetSizeWithCurrentAnchors((RectTransform.Axis)0, Mathf.Max(originalSize.x, LayoutUtility.GetPreferredSize(this.rectTransform, 0)+20));
+        else
+            this.rectTransform.SetSizeWithCurrentAnchors((RectTransform.Axis)0,LayoutUtility.GetPreferredSize(this.rectTransform, 0)+20);
+        
+        this.currentSize=this.rectTransform.sizeDelta;
+
+    }
+    void Awake(){
+        this.originalSize=this.rectTransform.sizeDelta;
+        this.currentSize=this.rectTransform.sizeDelta;
+    }
+
+    private RectTransform _rectTransform;
+    public RectTransform rectTransform{
+        get{
+            return _rectTransform??(_rectTransform=this.GetComponent<RectTransform>());
+        }
+    }
+    void OnEnable(){
+
+        inputField.onValueChanged.AddListener(OnValueChanged);
+        OnValueChanged("");
+    }
+    void OnDisable(){
+        inputField.onValueChanged.RemoveListener(OnValueChanged);
+
+        this.rectTransform.SetSizeWithCurrentAnchors((RectTransform.Axis)0, originalSize.x);
+
+    }
+}
